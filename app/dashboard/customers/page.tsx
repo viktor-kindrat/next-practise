f2de6525bb6d8bbabddd4d2b24d9871e4ad818a8
Viktor Kindrat <kindratvictor07@gmail.com>
2024-07-03 20:21:53 +0300

    Chapter 5 initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="Customers">
+            <p>Customers Page</p>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="Invoices">
+            <p>Invoices Page</p>
+        </div>
+    )
+}
